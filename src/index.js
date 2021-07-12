84715a5e67927d53171c1ca42f081af9af87de83
tta2yta <tta2yta@gmail.com>
2021-07-12 12:44:14 +0300

    Change entry point

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import _ from 'lodash';
  import './style.css';
 import Icon from './icon.png';
+import printMe from './print.js';
 
  function component() {
    const element = document.createElement('div');
+   const btn = document.createElement('button');
 
    // Lodash, now imported by this script
    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
    element.classList.add('hello');
 
   // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
+//   const myIcon = new Image();
+//   myIcon.src = Icon;
 
-  element.appendChild(myIcon);
+//   element.appendChild(myIcon);
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
    return element;
  }
